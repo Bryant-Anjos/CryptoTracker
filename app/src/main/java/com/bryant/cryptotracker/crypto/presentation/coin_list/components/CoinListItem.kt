f7990a30418c51a8925bde31f80cd5c785d44874
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2024-10-17 11:22:58 -0300

    create coin components

---
+++ app/src/main/java/com/bryant/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -0,0 +1,102 @@
+package com.bryant.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bryant.cryptotracker.crypto.domain.Coin
+import com.bryant.cryptotracker.crypto.presentation.models.CoinUi
+import com.bryant.cryptotracker.crypto.presentation.models.toCoinUi
+import com.bryant.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun CoinListItem(
+    coinUI: CoinUi,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = coinUI.iconRes),
+            contentDescription = coinUI.name,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(85.dp),
+        )
+        Column(
+            modifier = Modifier.weight(1f),
+        ) {
+            Text(
+                text = coinUI.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Text(
+                text = coinUI.name,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+        }
+        Column {
+            Text(
+                text = "$ ${coinUI.priceUsd.formatted}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            PriceChange(
+                change = coinUI.changePercent24Hr,
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListItemPreview() {
+    CryptoTrackerTheme {
+        CoinListItem(
+            coinUI = previewCoin,
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer),
+        )
+    }
+}
+
+internal val previewCoin = Coin(
+    id = "bitcoin",
+    rank = 1,
+    name = "Bitcoin",
+    symbol = "BTC",
+    marketCapUsd = 1241273958896.75,
+    priceUsd = 62828.15,
+    changePercent24Hr = -0.1,
+).toCoinUi()
